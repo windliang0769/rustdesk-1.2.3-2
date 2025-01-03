2c42da01c4a28b8219eb8019e8dfedf5722d7599
windliang0769 <77794078+windliang0769@users.noreply.github.com>
2025-01-03 11:24:10 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -103,7 +103,8 @@ const CHARS: &[char] = &[
 
 //pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 pub const RENDEZVOUS_SERVERS: &[&str] = &["as.egjfz.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+//pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const PUBLIC_RS_PUB_KEY: &str = "NkMKXsZHu1a9OBkbRnly7d8GfuYFt96BOWgqUloBlEA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
