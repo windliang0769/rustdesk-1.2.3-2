161e76f201de7fb7550001e146d533cff6b603ff
windliang0769 <35808241@qq.com>
2025-01-03 10:48:24 +0800

    1

--- flutter/ios_arm64.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cargo build --features flutter --release --target aarch64-apple-ios --lib
--- flutter/lib/models/platform_model.dart
+++
@@ -1,8 +0,0 @@
-import 'native_model.dart' if (dart.library.html) 'web_model.dart';
-import 'package:flutter_hbb/generated_bridge.dart'
-    if (dart.library.html) 'package:flutter_hbb/web/bridge.dart';
-
-final platformFFI = PlatformFFI.instance;
-final localeName = PlatformFFI.localeName;
-
-RustdeskImpl get bind => platformFFI.ffiBind;
